17a42461358079883be0d52d9a0961802916ad88
iYEiD <202110931@ua.edu.lb>
2023-12-17 13:14:06 +0200

    modified service.service

--- src/service/service.service.ts
+++ src/service/service.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@nestjs/common';
 import { CreateServiceDto } from './dto/create-service.dto';
 import { UpdateServiceDto } from './dto/update-service.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Service } from './entities/service.entity';
+import { Repository } from 'typeorm';
+import { find } from 'rxjs';
 
 @Injectable()
 export class ServiceService {
-  create(createServiceDto: CreateServiceDto) {
-    return 'This action adds a new service';
+  constructor(
+    @InjectRepository(Service)
+    private serviceRepository: Repository<Service>,
+  ) { }
+
+  async create(createServiceDto: CreateServiceDto): Promise<Service> {
+    const newService = this.serviceRepository.create(createServiceDto);
+    return this.serviceRepository.save(newService);
   }
 
-  findAll() {
-    return `This action returns all service`;
+  findAll(): Promise<Service[]> {
+    return this.serviceRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} service`;
+  findOne(id: number): Promise<Service> {
+    return this.serviceRepository.findOne({ where: { id: id } });
+  };
+
+
+  async update(id: number, updateServiceDto: UpdateServiceDto): Promise<Service> {
+    await this.serviceRepository.update(id, updateServiceDto);
+    return this.findOne(id);
   }
 
-  update(id: number, updateServiceDto: UpdateServiceDto) {
-    return `This action updates a #${id} service`;
-  }
+  async remove(id: number): Promise<void> {
+    const serviceToRemove = await this.findOne(id);
+    await this.serviceRepository.remove(serviceToRemove);
 
-  remove(id: number) {
-    return `This action removes a #${id} service`;
   }
 }
